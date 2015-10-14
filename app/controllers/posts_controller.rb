2e8113d5f80545dbdbc2fb1e378d6ee1d1bdd275
jrubyuncle <jrubyuncle@gmail.com>
2015-10-14 17:05:31 +0800

    add 'new' form and 'create' action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,18 @@ class PostsController < ApplicationController
   def show
     @post = Post.find params[:id]
   end
+
+  def new
+  end
+
+  def create
+
+    p = Post.new title: params[:title], content: params[:content]
+
+    if p.save
+      redirect_to post_path(p)
+    else
+      render :new
+    end
+  end
 end
