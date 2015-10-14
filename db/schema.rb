4634a39cf94f7272af03ed480621a2ac5c589ef2
jrubyuncle <jrubyuncle@gmail.com>
2015-10-14 14:42:14 +0800

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014062752) do
+ActiveRecord::Schema.define(version: 20151014063646) do
 
   create_table "posts", force: :cascade do |t|
     t.text "title"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20151014062752) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "nickname"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
