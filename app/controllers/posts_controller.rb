8ff27d1ab8dd4aa4f2a84a7aa1c4a662c71000f4
jrubyuncle <jrubyuncle@gmail.com>
2015-10-14 17:22:05 +0800

    user params.require.permit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,9 @@ class PostsController < ApplicationController
   def create
 
     #p = Post.new title: params[:title], content: params[:content]
-    p = Post.new params[:post].to_hash
+    #p = Post.new params[:post].to_hash
+    p = Post.new params.require(:post).permit(:title, :content)
+
 
     if p.save
       redirect_to post_path(p)
