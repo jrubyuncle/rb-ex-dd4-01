79b6cf7d2f821d0de65a4b31e9ec4328bd454bd5
jrubyuncle <jrubyuncle@gmail.com>
2015-10-14 15:26:09 +0800

    refactor: pass a function to before_create

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true, format: {with: /\A\d/}
+  before_create :gen_content
 
-  before_create do
+  def gen_content
     self.content = title if content.blank?
   end
 
