bcb0b1ffce2efdcc70e412293a24331111beb486
jrubyuncle <jrubyuncle@gmail.com>
2015-10-14 15:35:02 +0800

    before_save: change to upper case

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,16 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true, format: {with: /\A\d/}
   before_create :gen_content
+  before_save :upcase_all
 
+  protected
   def gen_content
     self.content = title if content.blank?
+  end
+
+  def upcase_all
+    title.try(:upcase!)
+    content.try(:upcase!)
   end
 
 end
